131330011b266b1f0c41d9adb77f536515403ce1
tadeuasarro <tadeutas@gmail.com>
2020-11-23 19:33:28 -0300

    Submit for a review

--- src/main/tabs/contact_tab.js
+++ src/main/tabs/contact_tab.js
@@ -23,16 +23,19 @@ const contact_tab = () => {
 
   const content = document.createElement('div');
 
+  const title = document.createElement('h3');
+  title.innerHTML = 'Contact';
+  title.classList.add('bg-dark', 'text-light', 'm-0', 'px-3', 'pt-3')
+
   const paragraph = document.createElement('p');
   paragraph.classList.add('mb-0', 'p-3', 'bg-dark', 'text-light', 'text-justify');
   paragraph.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lacinia massa ligula, egestas rutrum augue vehicula sed. Morbi luctus eros at mattis blandit. Donec imperdiet suscipit quam. Pellentesque feugiat pharetra blandit. Aenean vitae pharetra arcu. In non ipsum elementum nisi fermentum vulputate non nec eros. Praesent sagittis ante a blandit semper. Morbi sit amet ex consectetur, euismod dui dapibus, eleifend nunc.";
 
+  content.appendChild(title);
   content.appendChild(paragraph);
-
 
   container.appendChild(navbar);
   container.appendChild (content);
-
 
   contact.addEventListener('click', function(){
     const small_container = document.getElementById('small_container');
--- src/main/tabs/menu_tab.js
+++ src/main/tabs/menu_tab.js
@@ -23,16 +23,19 @@ const menu_tab = () => {
 
   const content = document.createElement('div');
 
+  const title = document.createElement('h3');
+  title.innerHTML = 'Menu';
+  title.classList.add('bg-dark', 'text-light', 'm-0', 'px-3', 'pt-3')
+
   const paragraph = document.createElement('p');
   paragraph.classList.add('mb-0', 'p-3', 'bg-dark', 'text-light', 'text-justify');
-  paragraph.innerHTML = "Some other content";
+  paragraph.innerHTML = "Nullam nisl risus, accumsan at justo et, dapibus tristique sem. Phasellus pulvinar congue erat at suscipit. Sed a dolor posuere, egestas eros sed, imperdiet mauris. Curabitur finibus vel sem efficitur mollis. Nam vitae ante justo. Sed sollicitudin auctor diam, iaculis ullamcorper augue consectetur quis. Mauris libero augue, suscipit quis lacus non, feugiat gravida ipsum. Curabitur vitae dignissim nunc, et vehicula sem. Proin iaculis fringilla orci eu scelerisque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam sit amet lorem pharetra, vehicula nibh bibendum, vulputate sapien. Cras faucibus libero at lectus mollis pellentesque.";
 
+  content.appendChild(title);
   content.appendChild(paragraph);
-
 
   container.appendChild(navbar);
   container.appendChild (content);
-
 
   contact.addEventListener('click', function(){
     const small_container = document.getElementById('small_container');
